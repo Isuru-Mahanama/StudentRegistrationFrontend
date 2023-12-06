162c89916fdd29b3d3d08dac877b66844d7c6758
unknown <isuruanamika@gmail.com>
2023-12-06 15:30:04 +0530

    Navbar component is build using tailwind

---
+++ src/app/Forms/personal-details-form/personal-details-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-personal-details-form',
+  templateUrl: './personal-details-form.component.html',
+  styleUrl: './personal-details-form.component.css'
+})
+export class PersonalDetailsFormComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './core/components/navbar/navbar.component';
+import { PersonalDetailsFormComponent } from './Forms/personal-details-form/personal-details-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    PersonalDetailsFormComponent,
+    
   ],
   imports: [
     BrowserModule,
